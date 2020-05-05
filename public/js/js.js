986f78a015644b4778db4fc592b8f4dffc7eb078
khanh <vohuykhanh2410@gmail.com>
2020-05-05 18:20:43 +0800

    first commit

---
+++ public/js/js.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    alert(document.getElementById("title-slider").style.position);
+}
+function myFunction() {
+    alert(document.getElementById("home-img").style.position);
+}
+function myFunction() {
+    alert(document.getElementById("music-img").style.position);
+}
+function myFunction() {
+    alert(document.getElementById("title-frames").style.position);
+}
+function myFunction() {
+    alert(document.getElementById("students").style.position);
+}
+function myFunction() {
+    alert(document.getElementById("courses").style.position);
+}
+function myFunction() {
+    alert(document.getElementById("hoursvideo").style.position);
+}
+function myFunction() {
+    alert(document.getElementById("frames-img-right").style.position);
+}
